ad0273dd17f8160d4d1d63f09f95eeba550515bf
KevinDang1 <kdang038@uottawa.ca>
2025-03-07 12:53:02 -0500

    contact us page begining

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,14 +1,36 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./index.css";
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 
 const ContactUs = () => {
-    return(
-        <div>
-            <Navbar />
-        </div>
-    );
+  return (
+    <div className="contact-container">
+  <h2 className="contact-title">Contact Us</h2>
+  
+  <div className="contact-content">
+    <div className="contact-form">
+      <label>First Name</label>
+      <input type="text" placeholder="First Name" />
+      
+      <label>Last Name</label>
+      <input type="text" placeholder="Last Name" />
+      
+      <label>Email Address</label>
+      <input type="email" placeholder="Email Address" />
+      
+      <label>Message (optional)</label>
+      <textarea placeholder="Enter Message"></textarea>
+      
+      <button className="send-button">Send Message</button>
+    </div>
+    
+    <div className="contact-map">
+      <img src="path-to-your-map-image.jpg" alt="Map" />
+    </div>
+  </div>
+</div>
+  );
 };
 
 export default ContactUs;
